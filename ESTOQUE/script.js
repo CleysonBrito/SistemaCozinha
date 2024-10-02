90dcd42c41190a2351cea5db863548808b4aa801
CleysonBrito <cleyson@reciclar.org.br>
2024-10-02 16:22:37 -0300

    Atualizar estoque

--- ESTOQUE/script.js
+++ ESTOQUE/script.js
@@ -3,12 +3,42 @@ function goHome() {
     window.location.href = './home.html';
 }
 
-// Função para buscar itens
-function searchItems() {
-    const searchInput = document.getElementById('searchInput').value;
-    const searchOption = document.getElementById('searchOptions').value;
-    // Lógica para pesquisar itens com base na opção selecionada e no valor de entrada
-    console.log(`Pesquisando por ${searchOption}: ${searchInput}`);
+// Função para buscar itens por data
+function searchItemsByDate() {
+    const startDate = document.getElementById('startDate').value;
+    const endDate = document.getElementById('endDate').value;
+
+    if (!startDate || !endDate) {
+        alert('Por favor, selecione ambas as datas.');
+        return;
+    }
+
+    const db = firebase.database().ref('entradaprodutos');
+    db.orderByChild('data_cadastro').startAt(startDate).endAt(endDate).on('value', (snapshot) => {
+        const data = snapshot.val();
+        console.log('Dados recebidos do Firebase:', data); // Adicione este log para verificar os dados recebidos
+        const tbody = document.getElementById('itemsTable').getElementsByTagName('tbody');
+        tbody.innerHTML = ''; // Limpa o conteúdo anterior
+
+        for (const key in data) {
+            if (data.hasOwnProperty(key)) {
+                const produto = data[key];
+                const row = tbody.insertRow();
+
+                row.insertCell(0).innerText = produto.sku;
+                row.insertCell(1).innerText = produto.descricao;
+                row.insertCell(2).innerText = produto.tipo;
+                row.insertCell(3).innerText = produto.unidade;
+                row.insertCell(4).innerText = produto.grupo;
+                row.insertCell(5).innerText = produto.quantidade;
+                row.insertCell(6).innerText = produto.fornecedor;
+                row.insertCell(7).innerText = produto.data_cadastro;
+                row.insertCell(8).innerText = produto.data_vencimento;
+                row.insertCell(9).innerText = produto.valor_unitario;
+                row.insertCell(10).innerText = produto.valor_total;
+            }
+        }
+    });
 }
 
 // Função para exportar para Excel
