e8b88c206f7b405337e04f7a5b781da29af9c97e
CleysonBrito <cleyson@reciclar.org.br>
2024-10-03 14:43:45 -0300

    Ajuste Codigo

--- CADASTRO_DE_PRODUTOS/app.js
+++ CADASTRO_DE_PRODUTOS/app.js
@@ -1,33 +1,19 @@
 document.getElementById('dataForm').addEventListener('submit', (e) => {
     e.preventDefault();
-    
-    // Cria o objeto com os dados do formulário
-    const data = {
-        sku: document.getElementById('sku').value,
-        descricao: document.getElementById('descricao').value,
-        tipo: document.getElementById('tipo').value,
-        unidade: document.getElementById('unidade').value,
-        grupo: document.getElementById('grupo').value,
-        quantidade: document.getElementById('quantidade').value,
-        fornecedor: document.getElementById('fornecedor').value,
-        data_cadastro: document.getElementById('data_cadastro').value,
-        data_vencimento: document.getElementById('data_vencimento').value,
-        aba: 'cadastrodeprodutos' // Especifica a aba correta
-    };
 
-    // Envia os dados como JSON para o Google Apps Script
+    // Cria o objeto FormData para coletar os dados do formulário
+    var formData = new FormData(document.getElementById('dataForm'));
+
+    // Envia os dados para o Google Apps Script
     fetch('https://script.google.com/macros/s/AKfycbzKX-uqS-ZZKcteIRU6vyrCk8Jlo2iYNdOKXLjmzYcCA7wZgbPabDvPlVFFVmjGdcpq/exec', {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json' // Define o tipo de conteúdo como JSON
-        },
-        body: JSON.stringify(data), // Envia os dados como JSON
+        body: formData
     })
-    .then(response => response.json()) // Converte a resposta para JSON
+    .then(response => response.json())  // Converte a resposta para JSON
     .then(data => {
         if (data.result === 'success') {
             alert('Produto salvo com sucesso!');
-            document.getElementById('dataForm').reset(); // Limpa o formulário
+            document.getElementById('dataForm').reset();  // Limpa o formulário
         } else {
             alert('Erro ao enviar os dados: ' + data.error);
         }
@@ -37,3 +23,13 @@ document.getElementById('dataForm').addEventListener('submit', (e) => {
         alert('Ocorreu um erro ao enviar os dados. Por favor, tente novamente.');
     });
 });
+
+// Função para limpar o formulário (se necessário)
+function limparFormulario() {
+    document.getElementById('dataForm').reset();  // Limpa o formulário
+}
+
+// Função para redirecionar para a home
+function irParaHome() {
+    window.location.href = './home.html';  // Substitua o caminho pela URL correta da sua home
+}
