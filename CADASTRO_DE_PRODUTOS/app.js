bdad64da3762313f762751314d99ba218b3e2e1b
CleysonBrito <cleyson@reciclar.org.br>
2024-10-04 12:17:29 -0300

    nova tentativa

--- CADASTRO_DE_PRODUTOS/app.js
+++ CADASTRO_DE_PRODUTOS/app.js
@@ -1,37 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('product-form');
+document.getElementById('supplier-form').addEventListener('submit', function(e) {
+    e.preventDefault();  // Evita o envio padrão do formulário
 
-    if (form) { // Verifica se o formulário existe no DOM
-        form.addEventListener('submit', function(e) {
-            e.preventDefault();  // Evita o envio padrão do formulário
+    var formData = new FormData(this);  // Coleta os dados do formulário
 
-            var formData = new FormData(form);  // Coleta os dados do formulário
-
-            // Substitua o URL pelo seu link do Google Apps Script correto
-            fetch('https://script.google.com/macros/s/AKfycbzKX-uqS-ZZKcteIRU6vyrCk8Jlo2iYNdOKXLjmzYcCA7wZgbPabDvPlVFFVmjGdcpq/exec', {
-                method: 'POST',
-                body: formData
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Erro na resposta do servidor');
-                }
-                return response.json();  // Transforma a resposta em JSON
-            })
-            .then(data => {
-                if (data.result === 'success') {
-                    alert('Dados enviados com sucesso!');
-                    form.reset();  // Limpa o formulário
-                } else {
-                    alert('Erro ao enviar os dados: ' + (data.error || 'Erro desconhecido'));
-                }
-            })
-            .catch(error => {
-                console.error('Erro:', error);
-                alert('Ocorreu um erro ao enviar os dados. Por favor, tente novamente.');
-            });
-        });
-    } else {
-        console.error('Formulário "product-form" não encontrado no DOM.');
-    }
+    // Substitua o URL pelo seu link do Google Apps Script
+    fetch('https://script.google.com/macros/s/AKfycbzKX-uqS-ZZKcteIRU6vyrCk8Jlo2iYNdOKXLjmzYcCA7wZgbPabDvPlVFFVmjGdcpq/exec', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())  // Transforma a resposta em JSON
+    .then(data => {
+        if (data.result === 'success') {
+            alert('Dados enviados com sucesso!');
+            document.getElementById('supplier-form').reset();  // Limpa o formulário
+        } else {
+            alert('Erro ao enviar os dados: ' + data.error);
+        }
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+        alert('Ocorreu um erro ao enviar os dados. Por favor, tente novamente.');
+    });
 });
+
+function goBack() {
+    window.history.back();  // Retorna para a página anterior
+}
