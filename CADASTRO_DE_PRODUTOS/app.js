450a7d557981095016bcf57f5849b9d91e6592ce
CleysonBrito <cleyson@reciclar.org.br>
2024-10-04 10:35:59 -0300

    java

--- CADASTRO_DE_PRODUTOS/app.js
+++ CADASTRO_DE_PRODUTOS/app.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('product-form');  // Garante que o formulário está sendo encontrado
+    const form = document.getElementById('product-form');
 
-    if (form) {
+    if (form) { // Verifica se o formulário existe no DOM
         form.addEventListener('submit', function(e) {
             e.preventDefault();  // Evita o envio padrão do formulário
 
             var formData = new FormData(form);  // Coleta os dados do formulário
 
+            // Substitua o URL pelo seu link do Google Apps Script correto
             fetch('https://script.google.com/macros/s/AKfycbzKX-uqS-ZZKcteIRU6vyrCk8Jlo2iYNdOKXLjmzYcCA7wZgbPabDvPlVFFVmjGdcpq/exec', {
                 method: 'POST',
                 body: formData
@@ -15,12 +16,12 @@ document.addEventListener('DOMContentLoaded', function() {
                 if (!response.ok) {
                     throw new Error('Erro na resposta do servidor');
                 }
-                return response.json();  // Converte a resposta para JSON
+                return response.json();  // Transforma a resposta em JSON
             })
             .then(data => {
                 if (data.result === 'success') {
                     alert('Dados enviados com sucesso!');
-                    form.reset();  // Limpa o formulário após o envio
+                    form.reset();  // Limpa o formulário
                 } else {
                     alert('Erro ao enviar os dados: ' + (data.error || 'Erro desconhecido'));
                 }
