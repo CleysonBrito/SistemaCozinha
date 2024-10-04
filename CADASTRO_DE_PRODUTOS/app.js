783aa782369ccd42127c9961f24a1a79e93dc708
CleysonBrito <cleyson@reciclar.org.br>
2024-10-04 09:58:10 -0300

    semdom

--- CADASTRO_DE_PRODUTOS/app.js
+++ CADASTRO_DE_PRODUTOS/app.js
@@ -1,37 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('product-form');
+form.addEventListener('submit', function(e) {
+    e.preventDefault();  // Evita o envio padrão do formulário
 
-    if (form) { // Verifica se o formulário existe no DOM
-        form.addEventListener('submit', function(e) {
-            e.preventDefault();  // Evita o envio padrão do formulário
+    var formData = new FormData(form);  // Coleta os dados do formulário
 
-            var formData = new FormData(form);  // Coleta os dados do formulário
-
-            // Substitua o URL pelo seu link do Google Apps Script correto
-            fetch('https://script.google.com/macros/s/AKfycbzKX-uqS-ZZKcteIRU6vyrCk8Jlo2iYNdOKXLjmzYcCA7wZgbPabDvPlVFFVmjGdcpq/exec', {
-                method: 'POST',
-                body: formData
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Erro na resposta do servidor');
-                }
-                return response.json();  // Transforma a resposta em JSON
-            })
-            .then(data => {
-                if (data.result === 'success') {
-                    alert('Dados enviados com sucesso!');
-                    form.reset();  // Limpa o formulário
-                } else {
-                    alert('Erro ao enviar os dados: ' + (data.error || 'Erro desconhecido'));
-                }
-            })
-            .catch(error => {
-                console.error('Erro:', error);
-                alert('Ocorreu um erro ao enviar os dados. Por favor, tente novamente.');
-            });
-        });
-    } else {
-        console.error('Formulário "product-form" não encontrado no DOM.');
+    // Exibe os dados coletados no console para depuração
+    for (var pair of formData.entries()) {
+        console.log(pair[0]+ ': ' + pair[1]);
     }
+
+    fetch('https://script.google.com/macros/s/AKfycbzKX-uqS-ZZKcteIRU6vyrCk8Jlo2iYNdOKXLjmzYcCA7wZgbPabDvPlVFFVmjGdcpq/exec', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro na resposta do servidor');
+        }
+        return response.json();  // Transforma a resposta em JSON
+    })
+    .then(data => {
+        if (data.result === 'success') {
+            alert('Dados enviados com sucesso!');
+            form.reset();  // Limpa o formulário
+        } else {
+            alert('Erro ao enviar os dados: ' + (data.error || 'Erro desconhecido'));
+        }
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+        alert('Ocorreu um erro ao enviar os dados. Por favor, tente novamente.');
+    });
 });
